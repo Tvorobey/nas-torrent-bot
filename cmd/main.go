5f84e9549d24b25ef637bc08cbeeb4582493db7d
evgen <egud.dev@gmail.com>
2024-08-01 23:35:19 +0300

    init use cases

--- cmd/main.go
+++ cmd/main.go
@@ -7,8 +7,10 @@ import (
 	"go.uber.org/zap/zapcore"
 	"log"
 	"nas-torrent-bot/internal/dig/config"
-	"nas-torrent-bot/internal/domain/fs_watcher"
+	"nas-torrent-bot/internal/domain/fs_manager"
 	"nas-torrent-bot/internal/domain/loader"
+	"nas-torrent-bot/internal/usecase/process_message"
+	"nas-torrent-bot/internal/usecase/send_message"
 	"os"
 	"os/signal"
 	"syscall"
@@ -47,24 +49,32 @@ func NewLogger(config *config.Config) (*zap.Logger, error) {
 func initDomains(container *dig.Container) error {
 	err := container.Provide(config.NewConfig)
 	err = container.Provide(NewLogger)
-	err = container.Provide(fs_watcher.New)
+	//err = container.Provide(fs_watcher.New)
 	err = container.Provide(loader.New)
+	err = container.Provide(fs_manager.New)
+
+	return err
+}
+
+func initUseCases(container *dig.Container) error {
+	err := container.Provide(send_message.New)
+	err = container.Provide(process_message.New)
 
 	return err
 }
 
 func digInvoke(ctx context.Context, container *dig.Container) error {
 	err := container.Invoke(func(cfg *config.Config, logger *zap.Logger) {
-		logger.Info("Application starting")
+		logger.Info("Application starting", zap.String("watchDir", cfg.WatchDir), zap.String("downloadDir", cfg.DownloadDir))
 	})
 
-	err = container.Invoke(func(watcher *fs_watcher.Watcher, cfg *config.Config, logger *zap.Logger) {
-		logger.Info("Starting watcher for", zap.String("dir", cfg.WatchDir))
-		err := watcher.Start(ctx)
-		if err != nil {
-			logger.Fatal("Failed to start watcher", zap.Error(err))
-		}
-	})
+	//err = container.Invoke(func(watcher *fs_watcher.Watcher, cfg *config.Config, logger *zap.Logger) {
+	//	logger.Info("Starting watcher for", zap.String("dir", cfg.WatchDir))
+	//	err := watcher.Start(ctx)
+	//	if err != nil {
+	//		logger.Fatal("Failed to start watcher", zap.Error(err))
+	//	}
+	//})
 
 	return err
 }
@@ -81,6 +91,11 @@ func main() {
 	err = digInvoke(ctx, container)
 	if err != nil {
 		log.Fatalf("failed invole dig: %v", err)
+	}
+
+	err = initUseCases(container)
+	if err != nil {
+		log.Fatalf("failed init usecases: %v", err)
 	}
 
 	// Grace shutdown
